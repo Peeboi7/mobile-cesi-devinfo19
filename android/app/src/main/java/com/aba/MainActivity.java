972bf6426b1a0e68128a41de7d41471334edb25a
Adam <adam.boussatha@viacesi.fr>
2020-10-07 16:52:16 +0200

    changements

--- android/app/src/main/java/com/aba/MainActivity.java
+++ android/app/src/main/java/com/aba/MainActivity.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.aba.app;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/aba/MainApplication.java
+++ android/app/src/main/java/com/aba/MainApplication.java
@@ -1,4 +1,4 @@
-package com.app;
+package com.aba.app;
 
 import android.app.Application;
 import android.content.Context;
